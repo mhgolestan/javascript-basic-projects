dcd82076485c422f9c217b2120ad9001221b9045
Mohammad Hossein Golestan <mohammad.h.golestan@ntnu.no>
2020-07-22 15:13:27 +0200

    color-flipper solved

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn')
+const color = document.querySelector(".color");
+
+document.addEventListener("click", function () {
+    let selectedColor = colors[randomGenerator()]
+    document.body.style.backgroundColor = selectedColor;
+    color.textContent = selectedColor;
+})
+
+function randomGenerator() {
+    return Math.floor(Math.random() * colors.length);
+}
--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,16 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn')
+const color = document.querySelector(".color");
+
+document.addEventListener("click", function () {
+    selectedColor = "#";
+    for (let i = 0; i < 6; i++) {
+        selectedColor += hex[randomGenerator()];
+    }
+    document.body.style.backgroundColor = selectedColor;
+    color.textContent = selectedColor;
+})
+
+function randomGenerator() {
+    return Math.floor(Math.random() * hex.length);
+}
